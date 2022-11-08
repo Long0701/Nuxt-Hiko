4d3a0ee7f3dc42886c571b696e7138e83ba01760
Long Nguyen <longnguyen@Longs-MacBook-Air.local>
2022-11-08 15:49:15 +0700

    Dist

--- nuxtjs/long-nuxtjs/.nuxt/components/index.js
+++ nuxtjs/long-nuxtjs/.nuxt/components/index.js
@@ -1,13 +1,13 @@
-export { default as NuxtLogo } from '../../components/NuxtLogo.vue'
-export { default as Tutorial } from '../../components/Tutorial.vue'
-export { default as CommonsAboutUs } from '../../components/commons/AboutUs.vue'
-export { default as CommonsCarousel } from '../../components/commons/Carousel.vue'
-export { default as CommonsDropdown } from '../../components/commons/Dropdown.vue'
-export { default as CommonsGotoTop } from '../../components/commons/GotoTop.vue'
-export { default as CommonsInput } from '../../components/commons/Input.vue'
-export { default as CommonsOurGames } from '../../components/commons/OurGames.vue'
-export { default as LayoutsFooter } from '../../components/layouts/footer.vue'
-export { default as LayoutsHeader } from '../../components/layouts/header.vue'
+export const NuxtLogo = () => import('../../components/NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
+export const Tutorial = () => import('../../components/Tutorial.vue' /* webpackChunkName: "components/tutorial" */).then(c => wrapFunctional(c.default || c))
+export const CommonsAboutUs = () => import('../../components/commons/AboutUs.vue' /* webpackChunkName: "components/commons-about-us" */).then(c => wrapFunctional(c.default || c))
+export const CommonsCarousel = () => import('../../components/commons/Carousel.vue' /* webpackChunkName: "components/commons-carousel" */).then(c => wrapFunctional(c.default || c))
+export const CommonsDropdown = () => import('../../components/commons/Dropdown.vue' /* webpackChunkName: "components/commons-dropdown" */).then(c => wrapFunctional(c.default || c))
+export const CommonsGotoTop = () => import('../../components/commons/GotoTop.vue' /* webpackChunkName: "components/commons-goto-top" */).then(c => wrapFunctional(c.default || c))
+export const CommonsInput = () => import('../../components/commons/Input.vue' /* webpackChunkName: "components/commons-input" */).then(c => wrapFunctional(c.default || c))
+export const CommonsOurGames = () => import('../../components/commons/OurGames.vue' /* webpackChunkName: "components/commons-our-games" */).then(c => wrapFunctional(c.default || c))
+export const LayoutsFooter = () => import('../../components/layouts/footer.vue' /* webpackChunkName: "components/layouts-footer" */).then(c => wrapFunctional(c.default || c))
+export const LayoutsHeader = () => import('../../components/layouts/header.vue' /* webpackChunkName: "components/layouts-header" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
